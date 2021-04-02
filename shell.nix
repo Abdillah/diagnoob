955f12e857c74cbcfefddda65162f4cb110f8820
Hernawan Fa'iz Abdillah <hernawan.faiz.abdillah@gmail.com>
2021-04-02 16:28:46 +0700

    Change deps

--- shell.nix
+++ shell.nix
@@ -3,9 +3,10 @@
 pkgs.mkShell {
   buildInputs = with pkgs; [
     python37Packages.pip
-    python37Packages.tensorflow-bin_2
+    python37Packages.tensorflow-build_2
+    python37Packages.tensorflow-tensorboard_2
     python37Packages.Keras
-    python37Packages.flask
+    #python37Packages.flask
 
     # keep this line if you use bash
     bashInteractive
