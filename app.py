99f056046c2ffeb83fd18bbc59ff05d020aa2428
mustofahisyam13 <mustofahisyam13@gmail.com>
2021-04-02 15:45:53 +0700

    adding template

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import pickle
 
 from flask import Flask
 from flask import jsonify, request
+from flask import render_template
 
 from tensorflow import keras
 from keras.preprocessing.text import Tokenizer
@@ -11,20 +12,21 @@ app = Flask("diagnoob")
 
 def preprocess(input):
     # create the tokenizer
-    f = open('./model/001-tokenizer.pickle', 'rb+')
-    t = pickle.load(f)
+    t = pickle.load('./model/001-tokenizer.pickle')
 
     # Pad the input vectors to ensure a consistent length
     x_input = np.array(t.texts_to_sequences(input))
     return pad_sequences(x_input, maxlen=500)
 
 def postprocess(output):
-    f = open('./model/001-labelencoder.pickle', 'rb+')
-    enc = pickle.load(f)
-
     output_classes = np.argmax(output, axis=None, out=None)
+    enc = pickle.load('./model/001-labelencoder.pickle')
     return enc.inverse_transform(output_classes)
 
+
+@app.route('/keyword-extract/')
+def keyword_extract():
+    return render_template('predict.html')
 
 @app.route('/')
 def hello_world():
